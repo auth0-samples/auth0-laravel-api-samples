bd87c8b9033fdd9463c84b59cfd8610c887dfd4a
German Lena <german.lena@gmail.com>
2016-09-15 17:51:23 -0300

    new laravel api seed project

---
+++ 00-Starter-Seed/app/Providers/AppServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Cache;
+use Auth0\Login\LaravelCacheWrapper;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            '\Auth0\Login\Contract\Auth0UserRepository',
+            '\Auth0\Login\Repository\Auth0UserRepository');
+
+        // This is used for RS256 tokens to avoid fetching the JWKs on each request
+        $this->app->bind(
+            '\Auth0\SDK\Helpers\Cache\CacheHandler',
+            function() {
+                static $cacheWrapper = null; 
+                if ($cacheWrapper === null) {
+                    $cache = Cache::store();
+                    $cacheWrapper = new LaravelCacheWrapper($cache);
+                }
+                return $cacheWrapper;
+            });
+    }
+}
---
+++ 00-Starter-Seed/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<pre>
+    
+    This application exposes one protected API endpoint:
+
+    GET /api/user
+
+    and receives the token in an authorization header:
+
+    Authorization: Bearer token
+
+</pre>
