121615fef3f28a90c092fe3ce737afc4d5e52af8
Alexis Luque <aluque@indexlabs.com.uy>
2018-05-23 14:12:50 -0300

    Add CORS support

---
+++ 01-Authorization-RS256/config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+
+    'supportsCredentials' => true,
+    'allowedOrigins' => ['http://localhost:3000'],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+
+];
